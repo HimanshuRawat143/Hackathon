ab964bf05b2e00436896e909ebbc69f35250fd09
Himanshu_Rawat <himu35311@gmail.com>
2025-04-08 20:50:47 +0530

    Footer updated !

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -57,7 +57,7 @@ const Footer = () => {
                   <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                   <polyline points="22,6 12,13 2,6"></polyline>
                 </svg>
-                <span>info@techhack2025.com</span>
+                <span>Hackaholic3.0.codev@gmail.com</span>
               </ContactItem>
               <ContactItem>
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
